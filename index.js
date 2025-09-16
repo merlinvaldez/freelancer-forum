83a0edcd18436f399b06f7e8c03c0019179d7783
Merlin Valdez <163349473+merlinvaldez@users.noreply.github.com>
2025-09-15 22:30:11 -0400

    completed up to instruction #2

--- index.js
+++ index.js
@@ -10,3 +10,30 @@ const NAMES = ["Alice", "Bob", "Carol", "Dave", "Eve"];
 const OCCUPATIONS = ["Writer", "Teacher", "Programmer", "Designer", "Engineer"];
 const PRICE_RANGE = { min: 20, max: 200 };
 const NUM_FREELANCERS = 100;
+
+function genFreelancer() {
+  const name = NAMES[Math.floor(Math.random() * NAMES.length)];
+  const occupation =
+    OCCUPATIONS[Math.floor(Math.random() * OCCUPATIONS.length)];
+  const rate =
+    PRICE_RANGE[
+      Math.floor(Math.random() * (PRICE_RANGE.max - PRICE_RANGE.min + 1)) +
+        PRICE_RANGE.min
+    ];
+  return { name, occupation, rate };
+}
+
+const freelancers = Array.from({ length: NUM_FREELANCERS }, genFreelancer);
+
+console.log(freelancers);
+
+function render() {
+  const $app = document.querySelector("#app");
+  $app.innerHTML = `
+   <h1>Freelancer Forum</h1>
+   <p> The average rate is TBD</p>
+   <tbody id="FreelancerRows"></tbody>
+`;
+  $app.querySelector("#FreelancerRows").replaceWith(FreelancerRows());
+}
+render();
